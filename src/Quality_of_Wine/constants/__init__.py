e8593de3ea108668735aa738fe7e1566b0932959
Ranjeth Aswin Ravindran <cecsranjethaswinr23@gmail.com>
2026-03-01 15:40:55 +0530

    problem in the 01_data_ingestion.ipynb , the zipfile erroe and config/config.yaml not found error frequent appears. for a time it worked and created a artifacts and a data.zip in it but the unzipped file did not run

--- src/Quality_of_Wine/constants/__init__.py
+++ src/Quality_of_Wine/constants/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
-SCHEMA_FILE_PATH = Path("schema.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")#should not be left as an empty file before execution
+SCHEMA_FILE_PATH = Path("schema.yaml")#should not be left as an empty file before execution
