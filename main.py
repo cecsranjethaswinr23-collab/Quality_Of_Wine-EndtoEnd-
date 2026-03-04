f14e520a4513a421256d3ee2f893676be019e896
Ranjeth Aswin Ravindran <cecsranjethaswinr23@gmail.com>
2026-03-04 14:37:50 +0530

    the data ingestion modular coding steps are noted in the research/01_data_ingestion.py and ran the code in main.py creating artifacts

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# also can be written as from src.Quality of Wine import 
+# logging(that is why -e. is used for not using the from src.Quality of Wine import logging)
 from Quality_of_Wine import logger
-#also can be written as from src.Quality of Wine import logging(that is why -e. is used for not using the from src.Quality of Wine import logging)
 
-#this import is after the updation of the stage_01_data_ingestion.py with pipeline codes
+#these imports is after the updation of the stage_01_data_ingestion.py with pipeline codes
 from Quality_of_Wine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Quality_of_Wine.config.configuration import ConfigurationManager
 from Quality_of_Wine.components.data_ingestion import DataIngestion
--- src/Quality_of_Wine/pipeline/stage_01_data_ingestion.py
+++ src/Quality_of_Wine/pipeline/stage_01_data_ingestion.py
@@ -3,20 +3,24 @@ from Quality_of_Wine.components.data_ingestion import DataIngestion
 from Quality_of_Wine import logger
 
 
-STAGE_NAME="Data Ingestion Stage"
-
-class DataIngestionTrainingPipeline: 
-    def __init__(self):
-        pass
-
-    def main(self): #this function initializes the donload and extraction of zip file
+STAGE_NAME="Data Ingestion Stage"       # ***these are the 
+                                        #    code that must
+class DataIngestionTrainingPipeline:    #    be written
+    def __init__(self):                 #    before giving
+        pass                            #    pipeline codes***
+    
+    #pipeline code
+    def main(self):                     #this function initializes the download and extraction of zip file
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
 
+"""
+this is the where code is actually(pipeline) is initaited
 
+"""
 if __name__ == '__main__':
     try:
         logger.info(f">>>>stage {STAGE_NAME} started<<<<")
@@ -27,8 +31,7 @@ if __name__ == '__main__':
         logger.excpetion(e)
 
 """
-
-next from here to test the pipeline , it has to be mentioned in 
+To execute the function, it has to be mentioned in 
 the main.py
 
 """
