84590729dfa12504a22a905ceddefd3190ce02a1
Ranjeth Aswin Ravindran <cecsranjethaswinr23@gmail.com>
2026-04-09 17:06:19 +0530

    app.py updated

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 
+# this must import from the prediction.py to run the app.py
+from Quality_of_Wine.pipeline.prediction import PredictionPipeline
+
+
